94a7e85b3fb79a3361d09eb6379d35d5124feb8c
gurdeepsainipb65 <gurdeepsainig2001@gmail.com>
2025-05-14 16:22:43 +0530

    updated

--- src/component/language/languagetranslator.jsx
+++ src/component/language/languagetranslator.jsx
@@ -7,22 +7,25 @@ export default function LanguageSwitcher() {
         new window.google.translate.TranslateElement(
           {
             pageLanguage: "en",
-            includedLanguages: "en,hi,de,fr,es,it,pt,da,sv,no,nl,tr,he,pl,ro,el,hu,ga,lt,lb,ru", 
-            layout: window.google.translate.TranslateElement.InlineLayout.HORIZONTAL,
+            includedLanguages:
+              "en,hi,de,fr,es,it,pt,da,sv,no,nl,tr,he,pl,ro,el,hu,ga,lt,lb,ru",
+            layout:
+              window.google.translate.TranslateElement.InlineLayout.HORIZONTAL,
           },
           "google_translate_element"
         );
       };
 
       const script = document.createElement("script");
-      script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+      script.src =
+        "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
       script.async = true;
       document.body.appendChild(script);
     }
   }, []);
 
   return (
-    <div className="absolute top-8 right-0">
+    <div className="absolute top-8 -right-13">
       <div id="google_translate_element"></div>
     </div>
   );
